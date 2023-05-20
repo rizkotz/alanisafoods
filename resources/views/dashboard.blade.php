0b1314253100376ea1408135e141c06b11e3fc45
MuhammadSetyaBudiPratama <msetyapratama27@gmail.com>
2023-05-20 18:37:47 +0700

    edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="{{ asset ('style/style.css') }}">
-    <link rel="stylesheet" href="{{ asset ('style/styleguide.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logotoko2.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('style/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('style/styleguide.css') }}">
 </head>
 
 <body>
@@ -17,60 +18,73 @@
         <div class="rectangle-3 rectangle">
         </div>
         <div class="alanisafoods">Alanisafoods</div>
-        <img class="menu-rounded" src="{{ asset ('img/Menu Rounded.png') }}" alt="Menu Rounded" />
-        <div class="logout-atas poppins-normal-black-16px" href="index.html">LOG OUT</div>
+        <img class="menu-rounded" src="{{ asset('img/Menu Rounded.png') }}" alt="Menu Rounded" />
+        <img class="male-user" src="img/Male User (1).png" alt="Male User" />
         <div class="user-admin poppins-normal-black-16px">User Admin</div>
-        <img class="logout" src="img/Logout.png" alt="Logout" /><img class="male-user" src="img/Male User (1).png"
-            alt="Male User" />
+        <a href="/index">
+            <img class="logout" src="img/Logout.png" alt="Logout" />
+            <div class="logout-atas poppins-normal-black-16px">LOG OUT</div>
+        </a>
         <div class="container">
             <div class="rectangle-2 rectangle">
                 <img class="male-user-1" src="img/Male User.png" alt="Male User" />
                 <div class="user-admin-1">User Admin </div>
-                <div class="total-barang">
-                    <img class="file" src="img/File.png" alt="File" />
-                    <div class="total-barang-1 poppins-normal-white-18px">Total Barang</div>
-                </div>
-                <div class="barang-masuk barang">
-                    <img class="reply-arrow" src="img/Reply Arrow.png" alt="Reply Arrow" />
-                    <div class="barang-masuk-1 poppins-normal-white-18px">Barang Masuk</div>
-                </div>
-                <div class="barang-keluar barang">
-                    <img class="right-2" src="img/Right 2.png" alt="Right 2" />
-                    <div class="barang-keluar-1 poppins-normal-white-18px">Barang Keluar</div>
-                </div>
-                <div class="data-transaksi">
-                    <img class="online-payment" src="img/Online Payment.png" alt="Online Payment" />
-                    <div class="data-transaksi-1 poppins-normal-white-18px">Data Transaksi</div>
-                </div>
-
-                <div class="tambah-barang">
-                    <img class="add-file" src="img/Add File.png" alt="Add File" />
-                    <div class="tambah-barang-1 poppins-normal-white-18px">Tambah Barang</div>
-                </div>
-                <div class="log-out-1">
-                    <img class="logout-1" src="img/Logout (1).png" alt="Logout" />
-                    <div class="logout-2 poppins-normal-white-18px">LOGOUT</div>
-                </div>
+                <a href="/totalBarang">
+                    <div class="total-barang">
+                        <img class="file" src="img/File.png" alt="File" />
+                        <div class="total-barang-1 poppins-normal-white-18px">Total Barang</div>
+                    </div>
+                </a>
+                <a href="/barangMasuk">
+                    <div class="barang-masuk barang">
+                        <img class="reply-arrow" src="img/Reply Arrow.png" alt="Reply Arrow" />
+                        <div class="barang-masuk-1 poppins-normal-white-18px">Barang Masuk</div>
+                    </div>
+                </a>
+                <a href="/barangKeluar">
+                    <div class="barang-keluar barang">
+                        <img class="right-2" src="img/Right 2.png" alt="Right 2" />
+                        <div class="barang-keluar-1 poppins-normal-white-18px">Barang Keluar</div>
+                    </div>
+                </a>
+                <a href="/dataTransaksi">
+                    <div class="data-transaksi">
+                        <img class="online-payment" src="img/Online Payment.png" alt="Online Payment" />
+                        <div class="data-transaksi-1 poppins-normal-white-18px">Data Transaksi</div>
+                    </div>
+                </a>
+                <a href="/tambahBarang">
+                    <div class="tambah-barang">
+                        <img class="add-file" src="img/Add File.png" alt="Add File" />
+                        <div class="tambah-barang-1 poppins-normal-white-18px">Tambah Barang</div>
+                    </div>
+                </a>
+                <a href="/index">
+                    <div class="log-out-1">
+                        <img class="logout-1" src="img/Logout (1).png" alt="Logout" />
+                        <div class="logout-2 poppins-normal-white-18px">LOGOUT</div>
+                    </div>
+                </a>
             </div>
 
             <div class="typemetric-stylestyle-container">
-                <a href="totalBarang">
-                <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 11.png"
-                    alt="Type=Metric. Style=Style 11" />
+                <a href="/totalBarang">
+                    <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 11.png"
+                        alt="Type=Metric. Style=Style 11" />
                 </a>
-                <a href="totalBarang">
-                <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 10.png"
-                    alt="Type=Metric. Style=Style 10" />
+                <a href="/barangMasuk">
+                    <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 10.png"
+                        alt="Type=Metric. Style=Style 10" />
+                </a>
+                <a href="/barangKeluar">
+                    <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 9.png"
+                        alt="Type=Metric. Style=Style 9" />
                 </a>
             </div>
             <div class="typemetric-stylestyle-1-container">
-                <a href="index.html">
-                <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 9.png"
-                    alt="Type=Metric. Style=Style 9" />
-                </a>
-                <a href="indedx.html">
-                <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 12.png"
-                    alt="Type=Metric. Style=Style 12" />
+                <a href="/tambahBarang">
+                    <img class="type-metric-style-style" src="img/Type=Metric. Style=Style 12.png"
+                        alt="Type=Metric. Style=Style 12" />
                 </a>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('dashboard');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/totalBarang', function () {
+    return view('totalBarang');
+});Route::get('/totalBarang', function () {
+    return view('totalBarang');
+});
+Route::get('/barangMasuk', function () {
+    return view('barangMasuk');
+});
+Route::get('/barangKeluar', function () {
+    return view('barangKeluar');
+});
+Route::get('/dataTransaksi', function () {
+    return view('dataTransaksi');
+});
+Route::get('/tambahBarang', function () {
     return view('tambahBarang');
 });
